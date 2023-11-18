8e56fa633c51f973cf8a1d4cc51203df46a7536b
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-18 19:14:26 +0900

    feat : 버스 기사 앱을 위한 빌드 프로젝트 생성 및 빌드 테스트

--- lower-bus-driver-app/App.js
+++ lower-bus-driver-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is a test</Text>
+      <Text>This is a test for IOS</Text>
       <StatusBar style="auto" />
     </View>
   );
