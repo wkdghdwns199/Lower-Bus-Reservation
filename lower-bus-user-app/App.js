a3f2a0d24dacef784d8a704e3b482593365b8420
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-21 05:20:17 +0900

    feat : 사용자 앱에 MainScreen, BusLineInfoScreen, BusReservationListScreen, Footer 경우에 맞게 나타나게 추가

--- lower-bus-user-app/App.js
+++ lower-bus-user-app/App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>This is a test</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import {StyleSheet, View, Text} from "react-native";
+import Footer from "./component/Footer";
+import MainScreen from "./screens/main/MainScreen";
+import BusReservationListScreen from "./screens/history/BusReservationListScreen";
+import BusLineInfoScreen from "./screens/reservation/BusLineInfoScreen";
+
+const App = () => {
+    const [currentScreen, setCurrentScreen] = useState('main');
+    const [reservationBusLine, setReservationBusLine] = useState('');
+    return (
+        <View style={styles.container}>
+            {currentScreen === 'main' &&
+                <MainScreen setCurrentScreen={setCurrentScreen} setReservationBusLine={setReservationBusLine}/>}
+            {currentScreen === 'busReservation' && <BusReservationListScreen/>}
+            {(currentScreen === 'main' || currentScreen === 'busReservation') &&
+                <Footer setCurrentScreen={setCurrentScreen}/>}
+            {currentScreen === 'busLineInfo' &&
+                <BusLineInfoScreen setCurrentScreen={setCurrentScreen} reservationBusLine={reservationBusLine}/>}
+        </View>
+    );
+};
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+
+export default App;
