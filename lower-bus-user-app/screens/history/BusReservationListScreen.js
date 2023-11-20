76da753ebfac1fcdb47b75df6969d01421e2fb91
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-21 05:22:07 +0900

    feat : 버스 예약 목록 기능 추가될 BusReservationListScreen 추가

---
+++ lower-bus-user-app/screens/history/BusReservationListScreen.js
@@ -0,0 +1,13 @@
+import {View, Text} from "react-native";
+
+const BusReservationListScreen = () => {
+    return (
+        <View>
+            <Text>Bus Reservation Screen</Text>
+        </View>
+    )
+
+}
+
+export default BusReservationListScreen
+
