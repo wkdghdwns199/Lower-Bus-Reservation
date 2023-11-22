8c75d66461b01a6027b3e6bf5b3bc0ed875554c6
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-23 04:40:21 +0900

    fix : babel.config.js syntax 에러 고침

--- lower-bus-user-app/babel.config.js
+++ lower-bus-user-app/babel.config.js
@@ -1,13 +1,15 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ['module:react-native-dotenv',{
-      "path": ".env",
-      "blacklist": null,
-      "whitelist": null,
-      "safe": false,
-      "allowUndefined": true
-    }],
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            ['module:react-native-dotenv', {
+                "path": ".env",
+                "blacklist": null,
+                "whitelist": null,
+                "safe": false,
+                "allowUndefined": true
+            }]
+        ]
+    };
 };
