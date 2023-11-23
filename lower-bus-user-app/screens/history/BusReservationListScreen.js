adc5cfaa9ca0f60d1339902786dc4f8013793988
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-23 10:58:45 +0900

    feat : 예약 내역 화면 준비 중 띄워놓기

--- lower-bus-user-app/screens/history/BusReservationListScreen.js
+++ lower-bus-user-app/screens/history/BusReservationListScreen.js
@@ -1,9 +1,12 @@
 import {View, Text} from "react-native";
-
+import {getData} from "../../component/AsyncStorage"
+import {useEffect} from "react";
 const BusReservationListScreen = () => {
+
+
     return (
         <View>
-            <Text>Bus Reservation Screen</Text>
+            <Text style={{fontSize:30,}}>준비 중...</Text>
         </View>
     )
 
