5f929ef9c2008fc0a5f00fb433495f73a183cd4d
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-23 10:57:44 +0900

    feat : 예약 화면, 하차 버튼 화면 기능 추가

--- lower-bus-user-app/App.js
+++ lower-bus-user-app/App.js
@@ -5,19 +5,37 @@ import Footer from "./component/Footer";
 import MainScreen from "./screens/main/MainScreen";
 import BusReservationListScreen from "./screens/history/BusReservationListScreen";
 import BusLineInfoScreen from "./screens/reservation/BusLineInfoScreen";
+import BusReservationScreen from "./screens/reservation/BusReservationScreen";
+import BusDepartureScreen from "./screens/departure/BusDepartureScreen";
 
 const App = () => {
     const [currentScreen, setCurrentScreen] = useState('main');
     const [reservationBusLine, setReservationBusLine] = useState('');
+    const [reservationBusCode, setReservationBusCode] = useState('');
+    const [startStation, setStartStation] = useState('');
+    const [showReservationModal, setShowReservationModal] = useState(false);
+    const [reservationUUID, setReservationUUID] = useState('');
     return (
         <View style={styles.container}>
             {currentScreen === 'main' &&
                 <MainScreen setCurrentScreen={setCurrentScreen} setReservationBusLine={setReservationBusLine}/>}
-            {currentScreen === 'busReservation' && <BusReservationListScreen/>}
-            {(currentScreen === 'main' || currentScreen === 'busReservation') &&
+            {currentScreen === 'busReservationList' && <BusReservationListScreen/>}
+            {(currentScreen === 'main' || currentScreen === 'busReservationList') &&
                 <Footer setCurrentScreen={setCurrentScreen}/>}
             {currentScreen === 'busLineInfo' &&
-                <BusLineInfoScreen setCurrentScreen={setCurrentScreen} reservationBusLine={reservationBusLine}/>}
+                <BusLineInfoScreen setCurrentScreen={setCurrentScreen} reservationBusLine={reservationBusLine}
+                                   setReservationBusCode={setReservationBusCode} setStartStation={setStartStation}
+                                   setShowReservationModal={setShowReservationModal}/>}
+            {currentScreen === 'busLineInfo' && showReservationModal &&
+                <BusReservationScreen setCurrentScreen={setCurrentScreen} reservationBusLine={reservationBusLine}
+                                      reservationBusCode={reservationBusCode} startStation={startStation}
+                                      setShowReservationModal={setShowReservationModal}
+                                      setReservationUUID={setReservationUUID}/>}
+
+            {currentScreen === 'busLDeparture' &&
+                <BusDepartureScreen setCurrentScreen={setCurrentScreen} reservationBusLine={reservationBusLine}
+                                    reservationBusCode={reservationBusCode}
+                                    setReservationUUID={setReservationUUID} reservationUUID={reservationUUID}/>}
         </View>
     );
 };
